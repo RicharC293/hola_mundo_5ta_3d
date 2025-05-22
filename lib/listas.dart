db692ddb28ab684d974ecf4ef6f21ae294759a97
Richar Cangui <richarsantiagocangui96@gmail.com>
2025-05-21 21:42:40 -0500

    first commit

---
+++ lib/listas.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Listas extends StatelessWidget {
+  const Listas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:hola_mundo/center.dart';
+import 'package:hola_mundo/layout.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _contador = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: CenterEjemplo(),
+      /*home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+          centerTitle: false,
+        ),
+        body: Center(child: Text('Hello World $_contador')),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _contador = _contador + 1;
+            setState(() {});
+          },
+          child: Icon(Icons.account_balance_sharp),
+        ),
+      ),*/
+    );
+  }
+}
